2a4e8b83014616b3bd5e50a005dfd3724b539f13
Dmitry Kosogorov <svinuga@gmail.com>
2023-07-30 14:19:17 +0400

    ДЗ "Крестики-нолики" выполнено

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -11,6 +11,7 @@
             void WriteGameField(string[] Field)
             {
                 Console.Clear();
+                Console.WriteLine();
                 Console.WriteLine(" --- --- --- ");
                 Console.WriteLine($"| {Field[0]} | {Field[1]} | {Field[2]} |");
                 Console.WriteLine(" --- --- --- ");
@@ -23,82 +24,121 @@
             void CheckWinner(string[] Field)
             {
 
-                if (Field[0] == Field[1] == Field[2])
-                Console.WriteLine($"Выиграли {State}");
-                return 1;
+                WriteGameField(Field);
+                if (string.Equals(Field[0], Field[1]) && string.Equals(Field[1], Field[2]))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Выиграл игрок играющий за {Player}");
+                    Status = 1;
+                    
+                }
 
-                if (Field[3] == State && Field[4] == State && Field[5] == State);
-                Console.WriteLine($"Выиграли {State}");
-                return 1;
+                if (string.Equals(Field[3], Field[4]) && string.Equals(Field[3], Field[5]))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Выиграл игрок играющий за {Player}");
+                    Status = 1;
+                    
+                }
 
-                if (Field[6] == State && Field[7] == State && Field[8] == State);
-                Console.WriteLine($"Выиграли {State}");
-                return 1;
+                if (string.Equals(Field[6], Field[7]) && string.Equals(Field[7], Field[8]))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Выиграл игрок играющий за {Player}");
+                    Status = 1;
+                    
+                }
 
-                if (Field[0] == State && Field[3] == State && Field[6] == State);
-                Console.WriteLine($"Выиграли {State}");
-                return 1;
+                if (string.Equals(Field[0], Field[3]) && string.Equals(Field[3], Field[6]))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Выиграл игрок играющий за {Player}");
+                    Status = 1;
+                    
+                }
 
-                if (Field[1] == State && Field[4] == State && Field[7] == State);
-                Console.WriteLine($"Выиграли {State}");
-                return 1;
+                if (string.Equals(Field[1], Field[4]) && string.Equals(Field[4], Field[7]))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Выиграл игрок играющий за {Player}");
+                    Status = 1;
+                    
+                }
 
-                if (Field[2] == State && Field[5] == State && Field[8] == State);
-                Console.WriteLine($"Выиграли {State}");
-                return 1;
+                if (string.Equals(Field[2], Field[5]) && string.Equals(Field[5], Field[8]))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Выиграл игрок играющий за {Player}");
+                    Status = 1;
+                    
+                }
 
-                if (Field[0] == State && Field[4] == State && Field[8] == State);
-                Console.WriteLine($"Выиграли {State}");
-                return 1;
+                if (string.Equals(Field[0], Field[4]) && string.Equals(Field[4], Field[8]))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Выиграл игрок играющий за {Player}");
+                    Status = 1;
+                    
+                }
 
-                if (Field[2] == State && Field[4] == State && Field[6] == State);
-                Console.WriteLine($"Выиграли {State}");
-                return 1;
-
-                return 0;
-
+                if (string.Equals(Field[2], Field[4]) && string.Equals(Field[4], Field[6]))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Выиграл игрок играющий за {Player}");
+                    Status = 1;
+                    
+                }
             }
 
             void CheckEnd(string[] Field)
             {
-                if (Field[0] != "1" && Field[1] != "2" && Field[2] != "3" && Field[3] != "4" && Field[4] != "5" && Field[5] != "6" && Field[6] != "7" && Field[7] != "8" && Field[8] != "9")
+                if (Field[0] != "1" && Field[1] != "2" && Field[2] != "3" && Field[3] != "4" && Field[4] != "5" && Field[5] != "6" && Field[6] != "7" && Field[7] != "8" && Field[8] != "9" && Status != 1)
                   {
+                    Console.WriteLine();
                     Console.WriteLine("Ничья");
                     Status = 1; 
                   }
-                else 
-                  {
-                    Status = 0;
-                }
             }
 
-            void MakeMove(string Player)
+            void MakeMove(string NPlayer)
             {
+                Console.WriteLine();
                 Console.WriteLine($"Игрок играющий за {Player} введите номер клетки (от 1 до 9) для следующего хода :");
                 if (int.TryParse(Console.ReadLine(), out int CMove))
-                  {
+                {
                     if (CMove < 0 || CMove > 9)
+                    {
+                        Console.WriteLine();
                         Console.WriteLine("Неправильный номер клетки");
+                    }
 
                     else if ((GameField[CMove - 1] == "X") || (GameField[CMove - 1] == "O"))
+                    {
+                        Console.WriteLine();
                         Console.WriteLine("Клетка занята");
+                    }
 
                     else
                     {
-                        GameField[CMove - 1] = Player;
-                        ChangePlayers(Player);
+                        GameField[CMove - 1] = NPlayer;
                     }
-                   }
+                }
                 else
+                {
+                    Console.WriteLine();
                     Console.WriteLine("Некорректный ввод");
+                }
                 
-                Console.ReadLine();
+                //Console.ReadLine();
             }
 
-            void ChangePlayers(string Player)
+            void ChangePlayers()
             {
                 if (Player == "X")
+                {
                     Player = "O";
+                    return;
+                }
                 else Player = "X";
             }
 
@@ -106,12 +146,11 @@
             {
                 WriteGameField(GameField);
                 MakeMove(Player);
-                Status = CheckWinner(GameField, Player);
+                CheckWinner(GameField);
                 CheckEnd(GameField);
+                ChangePlayers();
 
-            } while (Status == 1);
-            
-            Console.WriteLine(Status);
+            } while (Status == 0);
             
         }
         
